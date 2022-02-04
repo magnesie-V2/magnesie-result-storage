24a365d4225b8332f68e019759eff5308b8de86f
Mathieu MÉRILLON <mathieumeri@hotmail.fr>
2022-02-05 00:39:54 +0100

    Fusion of orchestrator and storages components in one docker-compose

--- database/0.1.0-01_table_creation.sql
+++
@@ -1,9 +0,0 @@
-USE `magnesie_result_storage`;
-
-CREATE TABLE IF NOT EXISTS `results` (
-    `id` INT PRIMARY KEY NOT NULL,
-    `model_path` VARCHAR(200) NOT NULL,
-    `texture_path` VARCHAR(200) NOT NULL
-);
-
-COMMIT;
